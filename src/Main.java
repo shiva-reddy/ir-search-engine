929f4f3c746464c6a475e8ee6fcd11310fbe80e8
Shiva Reddy <kjshiva94@gmail.com>
2021-05-06 03:06:22 -0500

    uncomment change

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,13 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        String filePath = new File("").getAbsolutePath();
-//        if(filePath.endsWith("src")){
-//            Constants.DEFAULT_CRAWL_DESITNATION = "../" + Constants.DEFAULT_CRAWL_DESITNATION;
-//            Constants.DEFAULT_KNOWLEDGE_BASE = "../" + Constants.DEFAULT_KNOWLEDGE_BASE;
-//            Constants.DEFAULT_STOPWORDS_PATH = "../" + Constants.DEFAULT_STOPWORDS_PATH;
-//        }
-//        ConsoleMenu menu = new ConsoleMenu();
-//        menu.run();
-        Evaluation.runEvaluations();
+        String filePath = new File("").getAbsolutePath();
+        if(filePath.endsWith("src")){
+            Constants.DEFAULT_CRAWL_DESITNATION = "../" + Constants.DEFAULT_CRAWL_DESITNATION;
+            Constants.DEFAULT_KNOWLEDGE_BASE = "../" + Constants.DEFAULT_KNOWLEDGE_BASE;
+            Constants.DEFAULT_STOPWORDS_PATH = "../" + Constants.DEFAULT_STOPWORDS_PATH;
+        }
+        ConsoleMenu menu = new ConsoleMenu();
+        menu.run();
     }
 }
